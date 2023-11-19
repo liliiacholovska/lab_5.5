381579da75e747a73e01fd722828a97ac80b4ea9
Ліля <chlvskalilia@gmail.com>
2023-11-19 13:50:10 +0200

    first test

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab5.5/lab5.5.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int n = 3;
+			int m = 9;
+			int r = n % m;
+			Assert::AreEqual(NSD(n, m), r);
+		}
+	};
+}
